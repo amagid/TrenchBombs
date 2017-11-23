5f48fbe8f7fe18e50860b6dd1d059ebf6b44894c
Aaron Magid <ahm64@case.edu>
2017-11-23 02:25:48 -0500

    feat(recipes): Added basic recipes for fluids.

--- prototypes/recipe.lua
+++ prototypes/recipe.lua
@@ -1,29 +1,70 @@
-data:extend({
-	{
-		type = "recipe",
-		name = TB_NAME,
-		enabled = true,
-		ingredients = {
-		},
-		result = TB_NAME,
+data: extend({
+    {
+        type = "recipe",
+            name = TB_NAME,
+            enabled = true,
+            ingredients = {},
+            result = TB_NAME,
+            energy_required = 0.1
+    },
+    {
+        type = "recipe",
+        name = TB_DETONATION_COORDINATOR_NAME,
+        enabled = true,
+        ingredients = {},
+        result = TB_DETONATION_COORDINATOR_NAME,
         energy_required = 0.1
-	},
-	{
-		type = "recipe",
-		name = TB_DETONATION_COORDINATOR_NAME,
-		enabled = true,
-		ingredients = {
-		},
-		result = TB_DETONATION_COORDINATOR_NAME,
+    },
+    {
+        type = "recipe",
+        name = TB_DETONATOR_NAME,
+        enabled = true,
+        ingredients = {},
+        result = TB_DETONATOR_NAME,
         energy_required = 0.1
-	},
-	{
-		type = "recipe",
-		name = TB_DETONATOR_NAME,
-		enabled = true,
-		ingredients = {
-		},
-		result = TB_DETONATOR_NAME,
-        energy_required = 0.1
-	}
+    },
+    {
+        type = "recipe",
+        name = TB_SHOTCRETE_NAME,
+        enabled = true,
+        energy_required = 5,
+        ingredients = {
+            {
+                type = "fluid", name = "water", amount = 100
+            },
+            {
+                name = "concrete",
+                amount = 10
+            }
+        },
+        results = {
+            {
+                type = "fluid", name = TB_SHOTCRETE_NAME, amount = 10
+            }
+        },
+        subgroup = "fluid-recipes"
+    },
+    {
+        type = "recipe",
+        name = TB_AMMONIA_NAME,
+        enabled = true,
+        energy_required = 1,
+        ingredients = {
+            {
+                type = "fluid", name = "hydrogen", amount = 30
+            },
+            {
+                type = "fluid", name = "nitrogen", amount = 10
+            },
+            {
+                name = "iron-plate", amount = 1
+            }
+        },
+        results = {
+            {
+                type = "fluid", name = TB_AMMONIA_NAME, amount = 20
+            }
+        },
+        subgroup = "fluid-recipes"
+    }
 })
